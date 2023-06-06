5141f48b5e6cf40dae55b0cbf50115808f0d8bc8
lucas.marques <lucasmarquesdevti@gmail.com>
2023-06-05 23:12:45 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto2/Program.cs
@@ -0,0 +1,22 @@
+﻿using Projeto2;
+
+Musica musica1 = new Musica()
+{
+    Nome = "Doce veneno",
+    Artista = "Costa gold",
+    Duracao = 300,
+    Disponivel = true
+};
+
+Musica musica2 = new Musica()
+{
+    Nome = "Conexões de mafia",
+    Artista = "Matuê",
+    Duracao = 450,
+    Disponivel = false
+};
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+
+
